acbe0cf67e35ba218da2e01c905a730251dcbe76
Rapturex23 <federico.ingenito@gmail.com>
2025-07-25 10:19:55 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandBlue: "#0077b6",
+        brandTeal: "#00b4d8"
+      }
+    },
   },
   plugins: [],
 };
