d3cf36c7ad2baadce9990500b66c7e7cc485cb23
Rapturex23 <federico.ingenito@gmail.com>
2025-07-27 12:11:59 +0200

    Update voli.js

--- api/voli.js
+++ api/voli.js
@@ -2,20 +2,15 @@ export default async function handler(req, res) {
   const { origin, destinazione, dataPartenza, dataRitorno } = req.query;
 
   try {
-    res.status(200).json({
-      success: true,
-      data: [
-        {
-          provider: "Skyscanner",
-          link: `https://www.skyscanner.it/trasporti/voli/${origin}/${destinazione}/${dataPartenza.replaceAll("-","")}/${dataRitorno.replaceAll("-","")}`
-        },
-        {
-          provider: "Kayak",
-          link: `https://www.kayak.it/flights/${origin}-${destinazione}/${dataPartenza}/${dataRitorno}`
-        }
-      ]
-    });
-  } catch (err) {
-    res.status(500).json({ success: false, error: err.message });
+    // DATI VOLI CON IMMAGINI E LINK (simulazione)
+    const voli = [
+      { compagnia: "Ryanair", prezzo: 150, durata: "1h50m", img: "https://picsum.photos/200/150?ryanair", link: "#" },
+      { compagnia: "Air France", prezzo: 200, durata: "2h00m", img: "https://picsum.photos/200/150?airfrance", link: "#" },
+      { compagnia: "EasyJet", prezzo: 180, durata: "1h55m", img: "https://picsum.photos/200/150?easyjet", link: "#" }
+    ];
+
+    res.status(200).json({ success: true, data: voli });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
   }
 }
